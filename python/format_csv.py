1007dda8fb283fe7da80fe919d9ad47286c834ee
Phil Serpa Schiavo <pserpaschiavo@gmail.com>
2024-10-29 17:41:51 -0300

    feat: add python scripts

---
+++ python/format_csv.py
@@ -0,0 +1,25 @@
+import csv
+
+# Nome do arquivo de entrada
+input_file = './datasets/unloaded-res.txt'
+
+# Ler os dados do arquivo .txt
+with open(input_file, 'r') as file:
+    data = file.readlines()
+
+# Processar os dados
+processed_data = []
+for line in data:
+    # Substituir barras por vírgulas, remover unidades de tempo e mensagens
+    line = line.replace('µs', '').strip()
+    line = line.replace('msg/s', '').strip()
+    line = line.replace('s', '').strip()
+    line = line.replace('/', ',').strip()
+    processed_data.append(line.split())
+
+# Salvar em um arquivo CSV
+with open('./datasets/unloaded-res.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    
+    # Escrever os dados processados
+    csvwriter.writerows(processed_data)
